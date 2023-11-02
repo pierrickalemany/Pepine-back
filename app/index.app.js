3e33c815cd4672030f740e178a727923d2f0e6a8
Pierrick Alemany <pierrick.alemany@oclock.school>
2023-11-02 12:02:18 +0100

    ajout cors

--- app/index.app.js
+++ app/index.app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routers/index.router.js';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
