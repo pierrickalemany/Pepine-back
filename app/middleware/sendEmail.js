53631aadf2f7f3e1543c593c6737ea603308c122
Pierrick Alemany <pierrick.alemany@oclock.school>
2024-04-17 15:44:58 +0200

    change password token url

--- app/middleware/sendEmail.js
+++ app/middleware/sendEmail.js
@@ -25,7 +25,7 @@ async function sendPasswordResetEmail(email, resetToken) {
       <h2>Poussez pas derrière</h2>
       <p>Bonjour,</p>
       <p>Vous avez demandé une réinitialisation de mot de passe. Cliquez sur le bouton ci-dessous pour réinitialiser votre mot de passe. :</p>
-      <a href="${process.env.URL_RESET_PASSWORD}?token=${resetToken}" style="background-color: #F79323; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">Réinitialiser le mot de passe</a>
+      <a href=""${process.env.URL_RESET_PASSWORD}/${resetToken}" style="background-color: #F79323; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">Réinitialiser le mot de passe</a>
       <p>Si vous n'avez pas demandé de réinitialisation de mot de passe, ignorez simplement cet e-mail.</p>
     `,
   };
